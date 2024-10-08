de3a4bf14ef9e4644f087bf1ea66cc939381494a
Rahul Kashyap <30754662+itisrmk@users.noreply.github.com>
2024-10-08 13:58:15 -0700

    Initial Commit
    My first flask commit for github

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+# route 1 (page 1) #landing page
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+#route 2 (page 2) #user
+@app.route('/user/<name>')
+def user(name):
+    name = request.args.get('username')
+    return render_template('user.html', username=name)
+
+#route 3 (page 3) #user
+@app.route('/stocks')
+def stocks():
+    stocks = ['Apple', 'Microsoft']
+    return render_template('stocks.html', stocks=stocks)
+
+
+
+if __name__ == "__main__": app.run(debug=True)
+
+
+
+
+
+
+# from flask import Flask
+# app = Flask(__name__)
+# @app.route("/") 
+# def hello_world(): 
+#     return "Hello, World!"
+# if __name__ == "__main__": app.run(debug=True)
+
+
+
+# @app.route('/user/<name>')
+# def user(name):
+#     username = request.args.get('username')
+#     return render_template('user.html', username=username)
+
+# @app.route('/stocks')
+# def stocks():
+#     stocks = ['Apple', 'Microsoft']
+#     return render_template('stocks.html', stocks=stocks)
